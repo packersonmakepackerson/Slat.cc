81c583df90ba1d02310679991e2ca3a39f490b6e
pro skidder <133696170+packersonmakepackerson@users.noreply.github.com>
2024-08-04 12:41:29 +0200

    Update console key.lua

--- console key.lua
+++ console key.lua
@@ -3,3 +3,23 @@ local key = loadstring(game:HttpGet('https://pastebin.com/raw/HeZSn2rs'))()
 
 
 function consolebegin()
+    rconsoleprint('@@WHITE@@')
+    rconsolename("Slat - Stop! key required")
+    rconsoleprint("Get Key: https://pastebin.com/raw/HeZSn2rs \n")
+    rconsoleprint("Enter Key")
+    input = rconsoleinput()
+
+
+    if input == key then
+        rconsoleclear()
+        rconsoleprint('@@LIGHT_GREEN@@'')
+        rconsolename("Roblox Slat")
+        rconsoleprint("Welcome!\n")
+
+    elseif input ~= key then
+        rconsoleprint("Incorrect Key"\n)
+        rconsoleprint("Incorrect Key, Please make sure you have the correct key")
+        rconsoleclear()
+        consolebegin()
+    end
+end
